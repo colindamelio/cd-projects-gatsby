d717258426d0db09ced45f8648e3f63bcc6d280e
colindamelio <colin.damelio@gmail.com>
2019-06-19 14:40:51 -0400

    fix casing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Layout from "../components/Layout";
+import Layout from "../components/layout";
 import Menu from "../components/Menu";
 import SEO from "../components/Seo";
 
